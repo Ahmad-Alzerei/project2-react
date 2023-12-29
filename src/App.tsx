f1af77bc0eadf9f0b1104dcae11eef624838371d
ahmad alzerei <ahmadzerie56@gmail.com>
2023-12-29 16:39:19 +0200

    custom modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
 import ProductCard from "./components/ProductCard";
+import Modal from "./components/ui/Modal";
 import { productList } from "./data";
-
+import { useState } from "react";
+import Button from "./components/ui/Button";
 function App() {
+  /*STATE*/
+  const [isOpen, setIsOpen] = useState(false);
+  /*HANDLER */
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+  /*RENDER */
   const renderProductList = productList.map((product) => (
     <ProductCard key={product.id} product={product} />
   ));
   return (
     <main className="container mx-auto">
+      <Button className="bg-indigo-700 hover:bg-indigo-800" onClick={openModal}>
+        ADD
+      </Button>
       <div className="m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-2 rounded-md">
         {renderProductList}
       </div>
+      <Modal isOpen={isOpen} closeModal={closeModal} title="ADD NEW PRODUCT">
+        <div className="flex items-center space-x-3">
+          <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
+          <Button className="bg-gray-300 hover:bg-gray-400">Cancel</Button>
+        </div>
+      </Modal>
     </main>
   );
 }
